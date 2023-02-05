0c58a65df981c45c504a84ebb6e8353ce7ccb1e5
MrFootwork <pandau.ting@gmail.com>
2023-02-05 19:39:28 +0100

    trying out new court-based db scheme

--- server/api/getData.ts
+++ server/api/getData.ts
@@ -1,37 +1,30 @@
-import type { IncomingMessage, ServerResponse } from 'http'
-
 import { MongoClient } from 'mongodb'
-import { config } from 'dotenv'
 
-// change in 3.0.0-rc.12
-// https://github.com/nuxt/framework/discussions/8296
-// Node.js/Express-style middleware with (req, res, next?) => {} signature
-// are not longer automatically converted to event handler format.
-// You can convert them using new fromNodeMiddleware((req, res) => {}) utility.
+export default defineEventHandler(async event => {
+	console.log('server is hit')
+	const query = getQuery(event)
+	// const body = await readBody(event)
+	const passwords = await fetchPasswords()
 
-export default fromNodeMiddleware(
-	async (req: IncomingMessage, res: ServerResponse) => {
-		const data = await fetchMongo()
+	// console.table(passwords)
 
-		// lazy writeHead: usually you would need to catch a 404
-		res.writeHead(200, { 'Content-Type': 'application/json' })
-		res.write(JSON.stringify(data))
-		res.end()
+	return {
+		api: 'works',
+		in: query,
+		out: passwords,
 	}
-)
+})
 
-async function fetchMongo() {
-	const uri = process.env.MONGODB_URI || ''
-	const mongoClient: MongoClient = new MongoClient(uri)
+async function fetchPasswords() {
+	const { mongoURI } = useRuntimeConfig()
+	const mongoClient: MongoClient = new MongoClient(mongoURI)
 
 	try {
 		await mongoClient.connect()
-		const db = mongoClient.db('food')
-		// TODO parallelize both promises
-		const dry = await db.collection('dry').find({}).toArray()
-		const wet = await db.collection('wet').find({}).toArray()
+		const db = mongoClient.db('security')
+		const keys = await db.collection('keys').find({}).toArray()
 
-		return [...dry, ...wet]
+		return keys
 	} catch (e) {
 		console.error('could not read from database. ', e)
 	} finally {
